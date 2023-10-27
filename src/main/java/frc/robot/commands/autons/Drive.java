613b0ea6bbf6c04453493dcc8c4c6bd8f9b039ee
JosephTLockwood <joeojazz@gmail.com>
2023-10-27 17:41:15 -0400

    Offseason

--- src/main/java/frc/robot/commands/autons/Drive.java
+++ src/main/java/frc/robot/commands/autons/Drive.java
@@ -1,11 +1,12 @@
 package frc.robot.commands.autons;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DriveXFeet;
 import frc.robot.subsystems.DriveTrain;
 
-public class Drive extends SequentialCommandGroup{
-    public Drive(DriveTrain drivetrain){
+public class Drive extends SequentialCommandGroup {
+    public Drive(DriveTrain drivetrain) {
         new DriveXFeet(6, drivetrain);
     }
-    
+
 }
